df8468add972401eefd618dc19d6760d313e1067
guy.peled <guy.peled@unity3d.com>
2024-01-15 19:36:29 +0100

    extract song

--- src/test.ts
+++ src/test.ts
@@ -2,12 +2,19 @@ import scrapeIt from "scrape-it";
 
 
 // Promise interface
-scrapeIt("https://ionicabizau.net", {
-    title: ".header h1"
-  , desc: ".header h2"
-  , avatar: {
-        selector: ".header img"
-      , attr: "src"
+scrapeIt("https://shironet.mako.co.il/artist?type=lyrics&lang=1&prfid=960&wrkid=6948", {
+    title: "h1.artist_song_name_txt", 
+    artist: "a.artist_singer_title",
+    words:{
+      listItem: "td > b:nth-of-type(1) > a.artist_normal_txt_black"
+    },
+    music:{
+      listItem: "td > b:nth-of-type(2) > a.artist_normal_txt_black"
+    },
+    lines: {
+      selector: "span.artist_lyrics_text",
+      how: "text",
+      convert: x => x.split("\n")
     }
 }).then(({ data, status }) => {
     console.log(`Status Code: ${status}`)
